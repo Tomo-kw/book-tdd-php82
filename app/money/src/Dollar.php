6d03a0adadee5e9f3a45eaadb1723cebe960bbcf
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2024-04-29 13:55:33 +0900

    TODO：$5 * 2 = $10 完了

--- app/money/src/Dollar.php
+++ app/money/src/Dollar.php
@@ -6,12 +6,14 @@ namespace Money;
 
 class Dollar
 {
-    public int $amount = 10;
+    public int $amount;
     public function __construct(int $amount)
     {
+        $this->amount = $amount;
     }
 
     public function times(int $multiplier): void
     {
+        $this->amount *= $multiplier;
     }
 }

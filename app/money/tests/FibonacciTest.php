b70241104be222172a233936dbedcc8153b2a138
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2024-08-18 18:33:41 +0900

    feat: fib(1) = 1

--- app/money/tests/FibonacciTest.php
+++ app/money/tests/FibonacciTest.php
@@ -10,10 +10,15 @@ final class FibonacciTest extends TestCase
 {
     public function fib(int $n): int
     {
-        return 0;
+        if($n === 0) {
+            return 0;
+        }
+
+        return 1;
     }
     public function testFibonacci()
     {
         $this->assertEquals(0, $this->fib(0));
+        $this->assertEquals(1, $this->fib(1));
     }
 }

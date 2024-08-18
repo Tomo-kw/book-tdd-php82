530cb3643fa95323bcce8791c4a8c0dfaea61723
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2024-08-18 19:41:35 +0900

    フィボナッチ数列の実装完了

--- app/money/tests/FibonacciTest.php
+++ app/money/tests/FibonacciTest.php
@@ -14,13 +14,17 @@ final class FibonacciTest extends TestCase
             return 0;
         }
 
-        return 1;
+        if ($n === 1) {
+            return 1;
+        }
+
+        return $this->fib($n - 1) + $this->fib($n - 2);
     }
     public function testFibonacci()
     {
         // [入力値, 期待される結果] のペア
         $cases = [
-            [0, 0],[1,1]
+            [0, 0],[1, 1],[2, 1],[3, 2],[4, 3],[5, 5],[6, 8],[7, 13],[8, 21],[9, 34]
         ];
 
         for ($i = 0; $i < count($cases); $i++) {
